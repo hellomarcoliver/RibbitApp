390c239e14b0ba316b6b9fe929cae593c0e8a0d5
Marc-Oliver <mo.gern@gmail.com>
2014-05-01 15:20:46 +0100

    Changed some comments. First change.

--- Ribbit/src/com/marcolivergern/ribbit/LoginActivity.java
+++ Ribbit/src/com/marcolivergern/ribbit/LoginActivity.java
@@ -29,7 +29,7 @@ public class LoginActivity extends Activity {
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present. Thats it.
+		// Inflate the menu; this adds items to the action bar if it is present. What if it works? Marc-Oliver.
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
