1c0b75a98c40d2cb6a88e29ae5878e24d5d6dada
Marc-Oliver <mo.gern@gmail.com>
2014-05-12 11:15:59 -0400

    Added functionality to push images and videos to parse.com

--- Ribbit/src/com/marcolivergern/ribbit/ParseConstants.java
+++ Ribbit/src/com/marcolivergern/ribbit/ParseConstants.java
@@ -2,9 +2,19 @@ package com.marcolivergern.ribbit;
 
 public final class ParseConstants {
 	// Class Name
+	public static final String CLASS_MESSAGES = "Messages"; // Classes in Parse begin with capital letter
 	
 	// Field Names
-	public static final String KEY_USERNAME = "username";
+	public static final String KEY_USERNAME = "username"; // fields begin with lower case letters
 	public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+	public static final String KEY_RECIPIENT_IDS = "recipientIds";
+	public static final String KEY_SENDER_ID = "senderId";
+	public static final String KEY_SENDER_NAME = "senderName";
+	public static final String KEY_FILE = "file";
+	public static final String KEY_FILE_TYPE = "fileType";
+	
+	public static final String TYPE_IMAGE = "image";
+	public static final String TYPE_VIDEO = "video";
+
 
 }
